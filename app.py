09fbbad5779a246fdcd6fc40fb84a843c2c2f3aa
Ruthvika2004 <147825236+Ruthvika2004@users.noreply.github.com>
2024-07-23 17:40:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def insert_chat_history(user_message, bot_response):
 genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 
 generation_config = {
-    "temperature": 0.5,
+    "temperature": 0.2,
     "top_p": 0.95,
     "top_k": 64,
     "max_output_tokens": 8192,
